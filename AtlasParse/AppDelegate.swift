69f593ab55d184b345b9fc5caa0b531dc76529ef
ysignal <375924709@qq.com>
2021-02-03 18:20:32 +0800

    Initial Commit

---
+++ AtlasParse/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AtlasParse
+//
+//  Created by user on 2021/2/3.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AtlasParse/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AtlasParse
+//
+//  Created by user on 2021/2/3.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
